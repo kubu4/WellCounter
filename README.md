a7d017a3f9c37656a29fda18fcd200c2cdb2edca
kubu4 <samuel.j.white@gmail.com>
2014-09-23 18:58:54 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+My first attempt at writing a program in Java.
+
+Program is designed to allow the user to count cells/larvae in wells of
+culture plates.
+
+Features:
+- Set maximum number of cells to count
+- Enter number of samples (wells) to be counted
+- Enter number of replicates for each sample (well)
+- Basic statistics on counts (running average, standard deviation) within replicates and/or across wells.
+
--- WellCounter.java
+++ WellCounter.java
@@ -4,9 +4,24 @@ public class WellCounter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Use Space Bar to tally larvae:");
+		int count = 0;
 		Scanner console = new Scanner(System.in);
-		String input = console.next();
+		int maxCount;
+		
+		System.out.print("Total larvae to count?");
+		maxCount = console.nextInt();
+		console.nextLine(); //Needed to clear new line character from previous console input.
+		System.out.println();
+		
+		System.out.println("Use Space Bar to tally larvae.");
+		System.out.print("Press Enter when finished.");
+		
+		String input = console.nextLine();
+		count = input.length();
+		
+		System.out.println();
+		System.out.println("You counted " + count + " larvae.");
+		System.out.println("You have " + (maxCount - count) + " remaining.");
 		
 	}
 
