d85e788cbdda1e48b1dc8e500a9e8849349c05d7
kubu4 <samuel.j.white@gmail.com>
2014-09-23 18:55:08 -0700

    Beginning of WellCounter.

---
+++ WellCounter.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class WellCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Use Space Bar to tally larvae:");
+		Scanner console = new Scanner(System.in);
+		String input = console.next();
+		
+	}
+
+}
