c72227322c7e42714318f1f129825048b717eab8
kubu4 <samuel.j.white@gmail.com>
2014-09-25 14:48:24 -0700

    Conensed into separate methods, added summation of replicate counts, added replicate incrementation, added while loop to iterate over counting while remaining amount to count is greater than 0.

--- WellCounter.java
+++ WellCounter.java
@@ -5,25 +5,43 @@ public class WellCounter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int count = 0;
+		int sum = 0;
 		Scanner console = new Scanner(System.in);
-		int maxCount;
-		int remaining = maxCount - count;
+		int maxCount = initialPrompt(console);
+		int remaining = maxCount;
+		int replicate = 0;
 		
+		while (remaining >= 0){
+			replicate++;
+			System.out.println("Replicate #" + replicate);
+			count = countingPrompt(console, count);
+			sum += count;
+			remaining = maxCount - sum;
+			System.out.println();
+			System.out.println("You counted " + count + " larvae.");
+			System.out.println("You have " + remaining + " remaining.");
+		
+		}
+	}
+		
+	public static int initialPrompt(Scanner console) {
+		int maxCount = 0;
 		System.out.print("Total larvae to count?");
 		maxCount = console.nextInt();
 		console.nextLine(); //Needed to clear new line character from previous console input.
 		System.out.println();
-		
+		return maxCount;
+	}
+	
+	
+	public static int countingPrompt(Scanner console, int count){
 		System.out.println("Use Space Bar to tally larvae.");
 		System.out.print("Press Enter when finished.");
-		
 		String input = console.nextLine();
 		count = input.length();
-		
-		System.out.println();
-		System.out.println("You counted " + count + " larvae.");
-		System.out.println("You have " + remaining + " remaining.");
-		
+		return count;
 	}
+	
+	
 
 }
