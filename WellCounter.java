0f89b36258c8b76f04938faa891b125737fa3ebf
kubu4 <samuel.j.white@gmail.com>
2014-09-25 22:04:59 -0700

    Added ArrayLists to store individual replicate numbers and corresponding count values.  Added methods to calculate mean and standard deviation.

--- WellCounter.java
+++ WellCounter.java
@@ -3,18 +3,22 @@ import java.util.*;
 public class WellCounter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		int count = 0;
 		int sum = 0;
 		Scanner console = new Scanner(System.in);
 		int maxCount = initialPrompt(console);
 		int remaining = maxCount;
 		int replicate = 0;
+		ArrayList<Integer>replicates = new ArrayList<Integer>(); //stores replicate number(s)
+		ArrayList<Integer> counts = new ArrayList<Integer>(); //stores counts that correspond to each replicate
 		
 		while (remaining >= 0){
 			replicate++;
+			add.replicates(replicate); //adds new replicate number to replicates ArrayList
 			System.out.println("Replicate #" + replicate);
 			count = countingPrompt(console, count);
+			add.counts(count); //adds new count value to the counts ArrayList
 			sum += count;
 			remaining = maxCount - sum;
 			System.out.println();
@@ -45,6 +49,31 @@ public class WellCounter {
 		return count;
 	}
 	
+	public double mean (ArrayList<Integer> counts){
+		double mean = 0;
+		int sum = 0;
+		
+		//for loop over each element in the counts ArrayList and adds them together as sum
+		for (int i = 0; i < counts.size(); i++){
+			sum += counts.get(i);
+		}
+		mean = sum/counts.size();
+		return mean;
+	}
 	
+	public double standardDeviation(double mean, ArrayList<Integer> counts){
+		double variance = 0;
+		double standardDeviation = 0;
+		int sum = 0;
+		
+		//for loop determines the difference from the mean for each element in the counts ArrayList,
+		//squares that value (within the Math.pow method), and then adds it to the previous value.
+		for (int i = 0; i < counts.size(); i++){
+			sum += Math.pow(counts.get(i) - mean, 2);
+		}
+		variance = sum/counts.size();//it's possible that this should actually be counts.size()-1 if counting a subset of population
+		standardDeviation = Math.sqrt(variance);
+		return standardDeviation;
+	}
 
 }
