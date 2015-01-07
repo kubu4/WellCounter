9637c21c3fcf3a91248295dcc8cfe7dbfd56c2e2
kubu4 <samuel.j.white@gmail.com>
2015-01-07 10:00:43 -0800

    Updated/improve commenting.

--- WellCounter.java
+++ WellCounter.java
@@ -17,20 +17,23 @@ public class WellCounter {
 		String keepCounting = "y";
 		
 		System.out.print("Do you want to set a target number to count? (y/n)");
-		targetChoice = console.next();
+		targetChoice = console.next(); //stores answer from question above
 		console.nextLine();
 		System.out.println();
+		
+		//takes answer from above and determines next user prompt
 		if (targetChoice.equals("y")){
 			System.out.print("Total to count?");
-			maxCount = console.nextInt();
+			maxCount = console.nextInt(); //stores answer from question above
 			console.nextLine(); //Needed to clear new line character from previous console input.
 			System.out.println();
 		}
 		
+		//while loop to store data entered if user wishes to keep counting after each entry
 		while (keepCounting.equals("y")){
 			count = countingPrompt(console);
 			counts.add(count); //adds each new count value to the counts ArrayList
-			replicate++;
+			replicate++; //increments each replicate after a counting event has been entered
 			replicates.add(replicate); //adds each new replicate number to replicates ArrayList
 			System.out.printf("Replicate #%d%n", replicate);
 			sum += count;
@@ -39,8 +42,8 @@ public class WellCounter {
 			double standardDeviation = standardDeviation(variance);
 			summary(targetChoice, count, sum, remaining, mean, standardDeviation, maxCount, keepCounting, console);
 			System.out.print("Keep counting? (y/n)");
-			keepCounting = console.next();
-			console.nextLine();
+			keepCounting = console.next(); //stores answer entered by user
+			console.nextLine(); //needed to clear new line character from previous console input
 			System.out.println();
 			if (keepCounting.equals("y")){
 				remaining = maxCount - sum;
@@ -52,9 +55,11 @@ public class WellCounter {
 	
 	
 	/*
-	 * A summary of each round of counting. that prints to the console.
-	 * Accepts the integer values: count, sum, remaining, and maxCount.
-	 * Accept the double values: mean, and standarDeviation.
+	 * A summary of each round of counting that prints to the console.
+	 * Accepts the integer parameters: count, sum, remaining, and maxCount.
+	 * Accepts the double parameters: mean and standarDeviation.
+	 * Accepts the String parameters: targetChoice and keepCounting.
+	 * Accepts the Scanner parameter: console
 	 */
 	public static void summary(String targetChoice, int count, int sum, int remaining, double mean, 
 			double standardDeviation, int maxCount, String keepCounting, Scanner console){
@@ -64,9 +69,12 @@ public class WellCounter {
 		if (targetChoice.equals("y")){
 			System.out.printf("You have %d remaining.%n", remaining);
 		}
+		
+		//informs user exceeded the value they entered for their maxCount
 		if (targetChoice.equals("y") && remaining < 0){
 			System.out.println("You counted " + Math.abs(remaining) + " over your " + maxCount);
 		}
+		
 		System.out.printf("The mean of your replicates is " 
 				+ "%.2f%n", mean); //prints mean to first two decimal places
 		System.out.printf("The standard deviation of your replicates is " 
@@ -78,14 +86,15 @@ public class WellCounter {
 	
 	/*
 	 * Counts the number of characters entered.  Does not distinguish between character type.
-	 * Accepts a Scanner for user input and the integer count.
-	 * Returns count.
+	 * i.e. user does not have to use the space bar to tally counts; any character entered will be counted.
+	 * Accepts a Scanner for user input.
+	 * Returns the Integer count.
 	 */
 	public static int countingPrompt(Scanner console){
 		System.out.println("Use Space Bar to tally larvae.");
 		System.out.print("Press Enter when finished.");
-		String input = console.nextLine();
-		int count = input.length();
+		String input = console.nextLine(); //stores the line that user entered at prompt
+		int count = input.length(); //stores the length of the line that user entered at prompt
 		System.out.println();
 		return count;
 	}
@@ -93,7 +102,7 @@ public class WellCounter {
 	
 	/*
 	 * Calculates the mean.
-	 * Accepts the ArrayList counts.
+	 * Accepts the parameter ArrayList counts.
 	 * Returns mean.
 	 */
 	public static double mean (ArrayList<Integer> counts){
@@ -102,7 +111,7 @@ public class WellCounter {
 		
 		//for loop over each element in the counts ArrayList and adds them together as sum
 		for (int i = 0; i < counts.size(); i++){
-			sum += counts.get(i);
+			sum += counts.get(i); //gets the value at each position in the ArrayList and sums them.
 		}
 		mean = sum/counts.size();
 		return mean;
@@ -111,7 +120,7 @@ public class WellCounter {
 	
 	/*
 	 * Calculates variance.
-	 * Accepts a mean and the ArrayList counts.
+	 * Accepts the double mean and the ArrayList counts as parameters.
 	 */
 	public static double variance(double mean, ArrayList<Integer> counts){
 		double variance = 0;
@@ -129,11 +138,11 @@ public class WellCounter {
 	
 	/*
 	 * Calculates the standard deviation.
-	 * Accepts variance.
+	 * Accepts the double variance as a parameter.
 	 */
 	public static double standardDeviation (double variance){
 		double standardDeviation = 0;
-		standardDeviation = Math.sqrt(variance);
+		standardDeviation = Math.sqrt(variance); //formula for calculating standard deviation
 		return standardDeviation;
 	}
 
